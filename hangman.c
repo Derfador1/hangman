e1ad2840bb2e09ede7e1ef7ae152386b617d7a14
Stephen Paradis <sparadis@localhost.localdomain>
2015-11-09 16:01:45 -0500

    Testing the printing of _ spots

--- hangman.c
+++ hangman.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
+#include <stdlib.h>
 
-
+#define CHANCES 5
+#define WORDS 5
 
 int main(int argc, char * argv[])
 {
+
+	srand(time(NULL));
+	int i;
+
 
 	if (argc == 1)
 	{
@@ -22,5 +29,16 @@ int main(int argc, char * argv[])
 
 	for (int c = 0; c < 5; c++)
 		printf("%s\n", dictionary[c]);
+
+	i = (rand() % WORDS);
+
+	char * word = dictionary[i];
+	size_t length = strlen(word);
+	for (unsigned int y = 0; y < length; y++)
+	{
+		printf("_ ");
+	}
+	printf("\nSize : %zd\n", length);
+	printf("%s\n", word);
 
 }
